d26eb95dbc0f688b3ec62633037588e9a92f0e6b
bravocalva <168801188+bravocalva@users.noreply.github.com>
2024-05-14 12:36:56 -0600

    ProyectoInicial

---
+++ control/web/js/main.js
@@ -0,0 +1,48 @@
+//alert("Hola Mundo");
+
+let adelante = document.getElementById("adelante");
+let atras = document.getElementById("atras");
+let derecha = document.getElementById("derecha");
+let izquierda = document.getElementById("izquierda");
+let detener = document.getElementById("detener");
+
+let mensaje = document.getElementById("mensaje");
+
+function callApi(estatus) {
+
+    // Hacer una petición para un usuario con ID especifico
+    axios.get("http://localhost/iot-car-control/back-end/apis/setRegistro.php?valorEstatus=" + estatus)
+        .then(function (response) {
+            // manejar respuesta exitosa
+            console.log(response);
+            mensaje.innerHTML = "Respuesta: <strong> " + response.data;
+        })
+        .catch(function (error) {
+            // manejar error
+            console.log(error);
+        })
+        .finally(function () {
+            // siempre sera executado
+        });
+
+}
+
+adelante.addEventListener("click", function () {
+    callApi('F');
+});
+
+atras.addEventListener("click", function () {
+    callApi('B');
+});
+
+derecha.addEventListener("click", function () {
+    callApi('R');
+});
+
+izquierda.addEventListener("click", function () {
+    callApi('L');
+});
+
+detener.addEventListener("click", function () {
+    callApi('S');
+});
